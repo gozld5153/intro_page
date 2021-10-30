8e014089103acab7baae03ccc432b77e3015c7c8
gozld5153 <gozld5153@naver.com>
2021-10-30 17:07:26 +0900

    intro page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
 
-function App() {
+
+const App = () => {
+  const hh = useRef()
+  const [idx, setIdx] = useState(0)
+  const [isHide,  setIsHide] = useState(true)
+  const [tic, setTic] = useState(true)
+
+
+  useEffect(() => {
+    if(idx < 4){
+      setTimeout(() => {
+        hh.current.children[idx].className = "fade"
+        setIdx(idx + 1)
+      }, 500)
+    }else {
+      setTimeout(() => {
+        if(tic)setTic(false)
+        else setTic(true)
+        setIsHide(false)
+      }, 800)
+    }
+  }, [idx, tic])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="title" >
+        <h1 ref={hh}>
+          <span>S</span>
+          <span>1</span>
+          <span>H</span>
+          <span>3</span>
+        </h1>
+      </div>
+      <div className="press">{tic ? null : "Press the Start Button"}</div>
+      <button className={isHide ? null : "start"}>Start</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
